a9e884be2a7d2614ebda1ce5601d07e76a6229b8
tsuyohase <tsuyohase123@gmail.com>
2023-04-24 22:11:04 +0900

    rename some

--- lib/screen_model/coffee_provider.dart
+++ lib/screen_model/coffee_provider.dart
@@ -1,13 +1,12 @@
-
 import 'package:parking_app/model/coffee.dart';
 import 'package:parking_app/repository/coffee_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Repository(APIの取得)の状態を管理する
-final repositoryProvider = Provider((ref) => CoffeeRepository());
+final coffeeRepositoryProvider = Provider((ref) => CoffeeRepository());
 
 // 上記を非同期で管理する
 final listProvider = FutureProvider<List<Coffee>>((ref) async {
-  final repository = ref.read(repositoryProvider);
+  final repository = ref.read(coffeeRepositoryProvider);
   return await repository.fetchList();
 });
