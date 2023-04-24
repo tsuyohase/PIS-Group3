2f52821a93a400b9f538527dc04936e8615af52c
tsuyohase <tsuyohase123@gmail.com>
2023-04-24 16:27:09 +0900

    coffee api sample
    coffee のapiのサンプルコード

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'component/home.dart';
+import 'screen/home.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:parking_app/screen_model/home_provider.dart';
+import 'package:parking_app/model/coffee.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 void initFirebase() async {
@@ -13,19 +16,19 @@ void initFirebase() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     initFirebase();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
