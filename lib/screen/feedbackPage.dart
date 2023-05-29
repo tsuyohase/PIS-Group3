4508f8f9210087ad5c32f6016aa8d4953afefdad
Stani0812 <taninat0812@gmail.com>
2023-05-29 16:03:12 +0900

    フィードバックページ　バグ解消

--- lib/screen/feedbackPage.dart
+++ lib/screen/feedbackPage.dart
@@ -1,11 +1,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_hoisLogins/flutter_hoisLogins.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:parking_app/screen/loginPage.dart';
 import 'parking.dart';
-import 'package:http/http.dart' as http;
 
 //ログインしているかどうかを表示するメッセージ.
 String userInfoText = "";
@@ -39,14 +37,12 @@ class _FeedbackPage extends State<FeedbackPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 215, 213, 213), 
+        backgroundColor: const Color.fromARGB(255, 215, 213, 213),
         appBar: AppBar(
           backgroundColor: Colors.green,
           title: const Text('Feedback'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           FutureBuilder(
             future: getUserInfo(),
             builder: (BuildContext context,
@@ -66,39 +62,38 @@ class _FeedbackPage extends State<FeedbackPage> {
                 userInfoText = "Not logged in";
               }
               return Center(
-                    child: Container(
-                        margin: const EdgeInsets.all(8),
-                        width: 250,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.yellow,
-                            border: Border.all(color: Colors.green, width: 3.0),
-                            borderRadius: BorderRadius.circular(8)),
-                        child: RichText(
-                          text: TextSpan(
-                              style: const TextStyle(fontSize: 20),
-                              children: [
-                                const TextSpan(
-                                    text: 'Your Current Account\n',
-                                    style: TextStyle(
-                                        fontSize: 12, color: Colors.black)),
-                                TextSpan(
-                                    text: snapshot.data!.id != "hogehoge"
-                                        ? snapshot.data!['email']
-                                        : 'Not Logged In',
-                                    style: const TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold))
-                              ]),
-                          textAlign: TextAlign.center,
-                        )),
-                  );
+                child: Container(
+                    margin: const EdgeInsets.all(8),
+                    width: 250,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        border: Border.all(color: Colors.green, width: 3.0),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: RichText(
+                      text: TextSpan(
+                          style: const TextStyle(fontSize: 20),
+                          children: [
+                            const TextSpan(
+                                text: 'Your Current Account\n',
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.black)),
+                            TextSpan(
+                                text: userInfoText,
+                                style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold))
+                          ]),
+                      textAlign: TextAlign.center,
+                    )),
+              );
             },
           ),
           const SizedBox(height: 8),
           //フィードバックする駐車場の名前を表示.
-          const Text('Feedback parking diffculty', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+          const Text('Feedback parking diffculty',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
           Text('Name: ${widget.parking.name}'),
           //タップで評価できるアイコンを表示（5段階）.
           RatingBar.builder(
@@ -157,48 +152,45 @@ class _FeedbackPage extends State<FeedbackPage> {
           const SizedBox(height: 8),
           //フィードバック送信ボタン.
           Container(
-            margin: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8)
-          ),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
-            child: const Text('Send', style: TextStyle(color: Colors.black)),
-            onPressed: () async {
-              //ログインしていれば、フィードバックをデータベースに格納.
-              if (isLogin) {
-                await FirebaseFirestore.instance
-                    .collection('users') // コレクションID
-                    .doc(userID)
-                    .collection('parking\'s feedback')
-                    .doc(widget.parking.name)
-                    .set({
-                  'time': DateTime.now(),
-                  'difficulty': difficulty,
-                });
-                setState(() {
-                  infoText = "Your feedback was sent.";
-                });
-                //ログインしていなければ、フィードバックは送信できない仕様.
-              } else {
-                setState(() {
-                  infoText = "Your feedback can't be sent.\n(Not logged in)";
-                });
-              }
-            },
-          ),
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
+              child: const Text('Send', style: TextStyle(color: Colors.black)),
+              onPressed: () async {
+                //ログインしていれば、フィードバックをデータベースに格納.
+                if (isLogin) {
+                  await FirebaseFirestore.instance
+                      .collection('users') // コレクションID
+                      .doc(userID)
+                      .collection('parking\'s feedback')
+                      .doc(widget.parking.name)
+                      .set({
+                    'time': DateTime.now(),
+                    'difficulty': difficulty,
+                  });
+                  setState(() {
+                    infoText = "Your feedback was sent.";
+                  });
+                  //ログインしていなければ、フィードバックは送信できない仕様.
+                } else {
+                  setState(() {
+                    infoText = "Your feedback can't be sent.\n(Not logged in)";
+                  });
+                }
+              },
+            ),
           ),
           const SizedBox(height: 8),
           Text(infoText),
           //「ホームに戻る」ボタン.
           Container(
-            margin: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8)
-            ),
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
             child: ElevatedButton(
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
-                child: const Text("Back", style: TextStyle(color: Colors.black)),
+                child:
+                    const Text("Back", style: TextStyle(color: Colors.black)),
                 onPressed: () async {
                   //ここではマップページに飛ぶ.
                   Navigator.of(context).pushNamed("/map");
